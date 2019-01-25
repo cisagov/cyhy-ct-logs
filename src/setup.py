162ecc5df5bdbd636321284fe933d3902bf5177e
Felddy <markf+github@geekpad.com>
2019-01-25 16:21:16 -0500

    add pymodm package

--- src/setup.py
+++ src/setup.py
@@ -28,6 +28,7 @@ setup(
         "dnspython",
         "python-dateutil >= 2.7.5",
         "pytest >= 4.1.1",  #TODO get pip install -e to pickup tests_require
+        "pymodm >= 0.4.1",
         "mock"
     ],
     # tests_require=[
