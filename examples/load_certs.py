22e75c8e668e9cbc9085a2b31c7b100b5c629eb5
Felddy <markf+github@geekpad.com>
2019-01-28 16:02:38 -0500

    split certs and precerts into separate collections

--- examples/load_certs.py
+++ examples/load_certs.py
@@ -29,7 +29,6 @@ DOMAIN = 'us-cert.gov'
 class Cert(MongoModel):
     log_id = fields.IntegerField(primary_key=True)
     serial = fields.CharField()  # 20 octets
-    precert = fields.BooleanField()
     issuer = fields.CharField()
     not_before = fields.DateTimeField()
     not_after = fields.DateTimeField()
@@ -39,10 +38,27 @@ class Cert(MongoModel):
     class Meta:
         indexes = [IndexModel(keys=[
             ('issuer', ASCENDING),
-            ('serial', ASCENDING),
-            ('precert', ASCENDING)], unique=True)]
+            ('serial', ASCENDING)], unique=True)]
         write_concern = WriteConcern(j=True)
         connection_alias = 'my-app'
+        final = True
+
+class PreCert(MongoModel):
+    log_id = fields.IntegerField(primary_key=True)
+    serial = fields.CharField()  # 20 octets
+    issuer = fields.CharField()
+    not_before = fields.DateTimeField()
+    not_after = fields.DateTimeField()
+    pem = fields.CharField()
+    subjects = fields.ListField(fields.CharField())
+
+    class Meta:
+        indexes = [IndexModel(keys=[
+            ('issuer', ASCENDING),
+            ('serial', ASCENDING)], unique=True)]
+        write_concern = WriteConcern(j=True)
+        connection_alias = 'my-app'
+        collection_name = 'precert'
         final = True
 
 
@@ -56,7 +72,6 @@ def make_cert_from_pem(pem):
         dns_names = set(san.get_values_for_type(x509.DNSName))
     except x509.extensions.ExtensionNotFound:  # craptacular interface you got there
         dns_names = set()
-
     try:
         x.extensions.get_extension_for_oid(
             x509.oid.ExtensionOID.PRECERT_POISON)
@@ -69,7 +84,11 @@ def make_cert_from_pem(pem):
     dns_names.add(cn)
 
     # build a Cert Model object
-    cert = Cert()
+    if precert_poison:
+        cert = PreCert()
+    else:
+        cert = Cert()
+
     cert.serial = hex(x.serial_number)[2:]
     cert.precert = precert_poison
     cert.issuer = x.issuer.rfc4514_string()
@@ -79,6 +98,14 @@ def make_cert_from_pem(pem):
     cert.subjects = dns_names
     return cert
 
+def cert_id_exists(log_id):
+    c = Cert.objects.raw({'_id': log_id})
+    if c.count() > 0:
+        return True
+    c = PreCert.objects.raw({'_id': log_id})
+    if c.count() > 0:
+        return True
+    return False
 
 def get_new_log_ids(domain):
     print(f'requesting certificate list for: {DOMAIN}')
@@ -88,8 +115,7 @@ def get_new_log_ids(domain):
         log_id = i['min_cert_id']
         print(f'processing log_id: {log_id}... ', end='')
         # check to see if we have this certificate already
-        c = Cert.objects.raw({'_id': log_id})
-        if c.count() > 0:
+        if cert_id_exists(log_id):
             # we already have it, skip
             print('skipping')
         else:
@@ -105,7 +131,8 @@ def main():
         cert = make_cert_from_pem(pem)
         cert.log_id = log_id
         cert.save()
-    #import IPython; IPython.embed()  # <<< BREAKPOINT >>>
+    import IPython
+    IPython.embed()  # <<< BREAKPOINT >>>
 
 
 if __name__ == '__main__':
