e50cc99e7343098acd907560fa4cb28594796e15
Felddy <markf+github@geekpad.com>
2019-02-04 14:29:02 -0500

    generalize and add arguments for document copy

--- examples/doc-copy.py
+++ examples/doc-copy.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+'''Mongo document copier.
+Be careful, this can easily give you a bad day.
+
+Usage:
+  doc-copy [options] <from_connection> <to_connection>
+  doc-copy (-h | --help)
+  doc-copy --version
+'''
 
 from models import Cert, Agency, Domain
 from util import connect_from_config
@@ -7,19 +15,32 @@ from pymodm import context_managers
 from tqdm import tqdm
 
 
+def copy_all(model, from_alias, to_alias):
+    print(f'Querying all {model} from {from_alias}')
+    with context_managers.switch_connection(model, from_alias):
+        all = model.objects.all()
+        #import IPython; IPython.embed()  # <<< BREAKPOINT >>>
+        print(f'Saving all {model} into {to_alias}')
+        for x in tqdm(all, total=all.count(), desc='Copy', unit='docs'):
+            with context_managers.switch_connection(model, to_alias):
+                x.save()
+
+
 def main():
+    from docopt import docopt
+    args = docopt(__doc__, version='v0.0.1')
+    from_alias = args['<from_connection>']
+    to_alias = args['<to_connection>']
+
+    # connect to databases
     connect_from_config()
+
     # Copy all documents from certs collection
-    all_certs = Cert.objects.all()
-    for cert in tqdm(all_certs, total=all_certs.count(), desc='Copy', unit='certs'):
-        with context_managers.switch_connection(Cert, 'production'):
-            cert.save()
+    copy_all(Cert, from_alias, to_alias)
+
     # Copy all documents from precerts collection
     with context_managers.switch_collection(Cert, 'precerts'):
-        all_certs = Cert.objects.all()
-        for cert in tqdm(all_certs, total=all_certs.count(), desc='Copy', unit='precerts'):
-            with context_managers.switch_connection(Cert, 'production'):
-                cert.save()
+        copy_all(Cert, from_alias, to_alias)
 
 
 if __name__ == '__main__':
