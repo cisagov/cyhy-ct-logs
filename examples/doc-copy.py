8724353ef93b1082fbea829c11933b773c29f42f
Felddy <markf+github@geekpad.com>
2019-02-04 17:22:24 -0500

    add option to list connections

--- examples/doc-copy.py
+++ examples/doc-copy.py
@@ -3,13 +3,18 @@
 Be careful, this can easily give you a bad day.
 
 Usage:
-  doc-copy [options] <from_connection> <to_connection>
+  doc-copy <from_connection> <to_connection>
+  doc-copy --list-connections
   doc-copy (-h | --help)
   doc-copy --version
+
+ Options:
+    -l --list-connections   List the available connections.
 '''
+import sys
 
 from models import Cert, Agency, Domain
-from util import connect_from_config
+from util import load_config, connect_from_config
 
 from pymodm import context_managers
 from tqdm import tqdm
@@ -19,16 +24,27 @@ def copy_all(model, from_alias, to_alias):
     print(f'Querying all {model} from {from_alias}')
     with context_managers.switch_connection(model, from_alias):
         all = model.objects.all()
-        #import IPython; IPython.embed()  # <<< BREAKPOINT >>>
         print(f'Saving all {model} into {to_alias}')
         for x in tqdm(all, total=all.count(), desc='Copy', unit='docs'):
             with context_managers.switch_connection(model, to_alias):
                 x.save()
 
 
+def print_connections():
+    config = load_config()
+    print('Connections in configuration: ')
+    for i in list(config['connections'].keys()):
+        print(f'\t{i}')
+
+
 def main():
     from docopt import docopt
     args = docopt(__doc__, version='v0.0.1')
+
+    if args['--list-connections']:
+        print_connections()
+        sys.exit(0)
+
     from_alias = args['<from_connection>']
     to_alias = args['<to_connection>']
 
