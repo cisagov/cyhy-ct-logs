c75fbfd5d771ddcbf9c1a6fe43e393784eb2b088
Felddy <markf+github@geekpad.com>
2019-02-11 18:40:46 -0500

    Flake8 all the things!

--- src/admiral/certs/tasks.py
+++ src/admiral/certs/tasks.py
@@ -1,13 +1,11 @@
-import sys
-import ipaddress
-import traceback
+"""Certificate Transparency Log Celery tasks."""
+
 import requests
 import json
 import re
 
-from celery import shared_task, states
+from celery import shared_task
 from celery.utils.log import get_task_logger
-from celery.exceptions import Ignore
 
 logger = get_task_logger(__name__)
 
@@ -18,21 +16,30 @@ DOMAIN_NAME_RE = re.compile(
 )
 
 
-@shared_task(autoretry_for=(Exception, requests.HTTPError, requests.exceptions.HTTPError),
+@shared_task(autoretry_for=(Exception, requests.HTTPError,
+                            requests.exceptions.HTTPError),
              retry_backoff=True,
              retry_jitter=True,
              retry_kwargs={'max_retries': 8})
 def summary_by_domain(domain, subdomains=True, expired=False):
+    """Fetch a summary of the certificates in the log.
+
+    Arguments:
+    domain -- the domain to query
+    subdomains -- include certificates of subdomains
+    expired -- include expired certificates
+    """
     # validate input
     m = DOMAIN_NAME_RE.match(domain)
-    if m == None:
+    if m is None:
         raise ValueError(f'invalid domain name format: {domain}')
 
     wildcard_param = '%.' if subdomains else ''
     expired_param = '' if expired else '&exclude=expired'
 
     logger.info(f'Fetching certs from CT log for: {wildcard_param}{domain}')
-    url = f'https://crt.sh/?Identity={wildcard_param}{domain}{expired_param}&output=json'
+    url = f'https://crt.sh/?Identity={wildcard_param}{domain}{expired_param}' \
+          f'&output=json'
     # TODO: make two queries
     req = requests.get(url, headers={'User-Agent': 'cyhy/2.0.0'})
 
@@ -47,11 +54,13 @@ def summary_by_domain(domain, subdomains=True, expired=False):
         req.raise_for_status()
 
 
-@shared_task(autoretry_for=(Exception, requests.HTTPError, requests.exceptions.HTTPError),
+@shared_task(autoretry_for=(Exception, requests.HTTPError,
+                            requests.exceptions.HTTPError),
              retry_backoff=True,
              retry_jitter=True,
              retry_kwargs={'max_retries': 8})
 def cert_by_id(id):
+    """Fetch a certificate by log ID."""
     logger.info(f'Fetching cert data from CT log for id: {id}.')
 
     url = f'https://crt.sh/?d={id}'
--- src/admiral/tester/tasks.py
+++ src/admiral/tester/tasks.py
@@ -1,20 +1,23 @@
+"""Test Celery tasks."""
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
 import time
 import random
-
 
 logger = get_task_logger(__name__)
 
 
 @shared_task
 def add(x, y):
+    """Add two numbers."""
     logger.info('Adding {0} + {1}'.format(x, y))
     return x + y
 
 
 @shared_task
 def slow_add(x, y):
+    """Add two numbers, slowly."""
     r = add(x, y)
     logger.info('Sleeping')
     time.sleep(5)
@@ -23,6 +26,7 @@ def slow_add(x, y):
 
 @shared_task
 def bad_add(x, y):
+    """Add two numbers, poorly."""
     r = slow_add(x, y)
     if random.choice([True, False]):
         logger.info('Failing on purpose')
@@ -36,16 +40,19 @@ def bad_add(x, y):
              retry_jitter=True,
              retry_kwargs={'max_retries': 3})
 def better_add(x, y):
+    """Add two number, just as poorly."""
     return bad_add(x, y)
 
 
 @shared_task
 def mul(x, y):
+    """Multiply two numbers."""
     logger.info('Multipling {0} + {1}'.format(x, y))
     return x * y
 
 
 @shared_task
 def xsum(numbers):
+    """Sum a list of numbers."""
     logger.info('Summing {0}'.format(numbers))
     return sum(numbers)
