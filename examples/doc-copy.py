e5c14cbf1cf4698cdcb42c63dfbb5003a260d530
Felddy <markf+github@geekpad.com>
2019-03-14 23:03:08 -0400

    Modify doc-copy to work with MongoEngine

--- examples/doc-copy.py
+++ examples/doc-copy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-'''Mongo document copier.
+"""Mongo document copier.
+
 Be careful, this can easily give you a bad day.
 
 Usage:
@@ -10,54 +11,61 @@ Usage:
 
  Options:
     -l --list-connections   List the available connections.
-'''
+"""
 import sys
 
-from models import Cert, Agency, Domain
-from util import load_config, connect_from_config
+from admiral.util import load_config, connect_from_config
 
-from pymodm import context_managers
+from mongoengine import context_managers
 from tqdm import tqdm
+
+from models import Domain
 
 
 def copy_all(model, from_alias, to_alias):
-    print(f'Querying all {model} from {from_alias}')
-    with context_managers.switch_connection(model, from_alias):
+    print(f"Querying all {model} from {from_alias}")
+    with context_managers.switch_db(model, from_alias) as model:
         all = model.objects.all()
-        print(f'Saving all {model} into {to_alias}')
-        for x in tqdm(all, total=all.count(), desc='Copy', unit='docs'):
-            with context_managers.switch_connection(model, to_alias):
-                x.save()
+        print(f"Saving all {model} into {to_alias}")
+        for x in tqdm(all, total=all.count(), desc="Copy", unit="docs"):
+            with context_managers.switch_db(model, to_alias) as model:
+                # MongoEngine tracks changes, we need to force the save
+                # or we'll get nothing.
+                x.save(force_insert=True)
 
 
 def print_connections():
     config = load_config()
-    print('Connections in configuration: ')
-    for i in list(config['connections'].keys()):
-        print(f'\t{i}')
+    print("Connections in configuration: ")
+    for i in list(config["connections"].keys()):
+        print(f"\t{i}")
 
 
 def main():
     from docopt import docopt
-    args = docopt(__doc__, version='v0.0.1')
 
-    if args['--list-connections']:
+    args = docopt(__doc__, version="v0.0.1")
+
+    if args["--list-connections"]:
         print_connections()
         sys.exit(0)
 
-    from_alias = args['<from_connection>']
-    to_alias = args['<to_connection>']
+    from_alias = args["<from_connection>"]
+    to_alias = args["<to_connection>"]
 
     # connect to databases
     connect_from_config()
 
-    # Copy all documents from certs collection
-    copy_all(Cert, from_alias, to_alias)
+    # # Copy all documents from certs collection
+    # copy_all(Cert, from_alias, to_alias)
+    #
+    # # Copy all documents from precerts collection
+    # with context_managers.switch_collection(Cert, "precerts"):
+    #     copy_all(Cert, from_alias, to_alias)
+    #
 
-    # Copy all documents from precerts collection
-    with context_managers.switch_collection(Cert, 'precerts'):
-        copy_all(Cert, from_alias, to_alias)
+    copy_all(Domain, from_alias, to_alias)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
